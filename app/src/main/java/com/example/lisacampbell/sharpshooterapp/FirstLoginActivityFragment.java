1896d91fa2fa0366cfe73165db36dae4ad57bda3
Lisa <campb215@purdue.edu>
2015-07-25 12:19:38 -0400

    Took out Google Play Services

---
+++ app/src/main/java/com/example/lisacampbell/sharpshooterapp/FirstLoginActivityFragment.java
@@ -0,0 +1,23 @@
+package com.example.lisacampbell.sharpshooterapp;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class FirstLoginActivityFragment extends Fragment {
+
+    public FirstLoginActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_first_login, container, false);
+    }
+}
